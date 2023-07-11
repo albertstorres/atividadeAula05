2332e1fbf67d7cabe4d8e9247bafc5e24102d3ef
albertstorres <albertstorres@gmail.com>
2023-07-11 14:49:46 -0300

    EXERCICIO 05. RESOLVIDO COMO PASSADO NA QUESTAO.

---
+++ exercicio05.js
@@ -0,0 +1,8 @@
+let distancia;
+let x1 = 1, x2 = 1;
+let y1 = 1, y2 = 4;
+
+distancia = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2);
+
+console.log(`DISTANCIA ENTRE OS DOIS PONTOS = ${distancia}`);
+
