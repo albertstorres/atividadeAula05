1cc2fdb868aea6f49013bcdb1fd3ccedfe6b3907
albertstorres <albertstorres@gmail.com>
2023-07-11 13:26:27 -0300

    EXERCICIO 01. CALCULADORA DE IMC.

---
+++ exercicio01/exercicio01.js
@@ -0,0 +1,7 @@
+let imc;
+let peso = 88;
+let altura = 1.75;
+
+imc = peso / altura ** 2;
+
+console.log(`IMC = ${imc}`);
---
+++ exercicio02/exercicio02.js
@@ -0,0 +1,6 @@
+let temperaturaFahrenheit = 73;
+let resultadoCelsius;
+
+resultadoCelsius = (temperaturaFahrenheit - 32) * (5 / 9);
+
+console.log(`${temperaturaFahrenheit} GRAUS FAHRENHEIT, É IGUAL A ${resultadoCelsius} GRAUS CELSIUS`);
